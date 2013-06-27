2fea93216ca2bdeed0d35d33b04e05d2cab36010
mik4041 <mik4041@yandex.ru>
2013-06-27 22:00:38 +0300

    some speed fixes

--- unity/Assets/person/person.cs
+++ unity/Assets/person/person.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class person : MonoBehaviour {
 	public Vector3 movement_speed;
 	public Vector3 jump_speed;
+	public float max_speed;
+	private bool touchingPlatform;
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -12,25 +15,31 @@ public class person : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		if(Input.GetButtonDown("Jump")) {
+		if(Input.GetButtonDown("Jump") && touchingPlatform) {
 			rigidbody.AddForce(jump_speed, ForceMode.VelocityChange);
+			touchingPlatform = false;
 		}
 		
 		if(Input.GetButton("Horizontal")) {
 			
-			if(Mathf.Abs(rigidbody.velocity.x) > 15) {
+			if (Mathf.Abs(rigidbody.velocity.x) > max_speed) {
 				return;
 			}
 			
-			if(Input.GetAxis("Horizontal") > 0) {
+			if (Input.GetAxis("Horizontal") > 0) {
 				rigidbody.AddForce(movement_speed, ForceMode.VelocityChange);
 			} else if (Input.GetAxis("Horizontal") < 0) {
 				rigidbody.AddForce(-movement_speed, ForceMode.VelocityChange);
 			}
 			
 		}
-		
+	}
+	
+	void OnCollisionEnter () {
+		touchingPlatform = true;
+	}
 
-		
+	void OnCollisionExit () {
+		touchingPlatform = false;
 	}
 }
